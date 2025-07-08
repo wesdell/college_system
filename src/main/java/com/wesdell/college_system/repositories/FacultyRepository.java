077b4bdd90925dec02a04ef75a8d653aefd8e2ff
Sebastian Guerrero <wesdell17@gmail.com>
2025-07-07 19:42:30 -0500

    New faculty repository

---
+++ src/main/java/com/wesdell/college_system/repositories/FacultyRepository.java
@@ -0,0 +1,10 @@
+package com.wesdell.college_system.repositories;
+
+import com.wesdell.college_system.models.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+    boolean existsByName(String name);
+}
