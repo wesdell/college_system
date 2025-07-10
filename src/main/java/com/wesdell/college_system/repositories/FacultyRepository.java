2cba8d95ec73cbf4c426e629071061c8c4ceaa35
Sebastian Guerrero <wesdell17@gmail.com>
2025-07-10 17:55:04 -0500

    Remove unnecessary code

--- src/main/java/com/wesdell/college_system/repositories/FacultyRepository.java
+++ src/main/java/com/wesdell/college_system/repositories/FacultyRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
-    boolean existsByName(String name);
 }
--- src/main/java/com/wesdell/college_system/repositories/SubjectRepository.java
+++ src/main/java/com/wesdell/college_system/repositories/SubjectRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject,Long> {
-    boolean existsByCode(String code);
 }
