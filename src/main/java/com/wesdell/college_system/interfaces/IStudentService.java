9e26dbf05dea7e3cadbe037e40ffe30b104b27dc
Sebastian Guerrero <wesdell17@gmail.com>
2025-08-12 10:58:18 -0500

    Define Student interface

--- src/main/java/com/wesdell/college_system/interfaces/IStudentService.java
+++ src/main/java/com/wesdell/college_system/interfaces/IStudentService.java
@@ -1,13 +1,14 @@
 package com.wesdell.college_system.interfaces;
 
+import com.wesdell.college_system.exceptions.ResourceNotFoundException;
 import com.wesdell.college_system.models.Student;
 
 import java.util.List;
 
 public interface IStudentService {
     List<Student> getStudents();
-    Student getStudentById(Long id);
+    Student getStudentById(Long id) throws ResourceNotFoundException;
     Student createStudent(Student newStudent);
-    Student updateStudentById(Long id, Student updatedStudent);
-    void deleteStudentById(Long id);
+    Student updateStudentById(Long id, Student updatedStudent) throws ResourceNotFoundException;
+    void deleteStudentById(Long id) throws ResourceNotFoundException;
 }
