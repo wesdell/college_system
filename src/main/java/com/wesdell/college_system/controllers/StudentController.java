b7fa44d621dca017fa6640e930b25cd52bc7adaa
Sebastian Guerrero <wesdell17@gmail.com>
2025-08-13 09:55:10 -0500

    Refactor name to follow java naming convention

--- src/main/java/com/wesdell/college_system/controllers/StudentController.java
+++ src/main/java/com/wesdell/college_system/controllers/StudentController.java
@@ -13,35 +13,35 @@ import java.util.List;
 @RequestMapping("api/v1/students")
 public class StudentController {
 
-    private final IStudentService istudentService;
+    private final IStudentService iStudentService;
 
-    public StudentController(IStudentService istudentService) {
-        this.istudentService = istudentService;
+    public StudentController(IStudentService iStudentService) {
+        this.iStudentService = iStudentService;
     }
 
     @GetMapping
     public ResponseEntity<List<Student>> getAllStudents() {
-        return new ResponseEntity<>(istudentService.getStudents(), HttpStatus.OK);
+        return new ResponseEntity<>(iStudentService.getStudents(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
-        return new ResponseEntity<>(istudentService.getStudentById(id), HttpStatus.OK);
+        return new ResponseEntity<>(iStudentService.getStudentById(id), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Student> createStudent(@Valid @RequestBody Student newStudent) {
-        return new ResponseEntity<>(istudentService.createStudent(newStudent), HttpStatus.CREATED);
+        return new ResponseEntity<>(iStudentService.createStudent(newStudent), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Student> updateStudent(@PathVariable Long id, @Valid @RequestBody Student newStudent) {
-        return new ResponseEntity<>(istudentService.updateStudentById(id, newStudent), HttpStatus.OK);
+        return new ResponseEntity<>(iStudentService.updateStudentById(id, newStudent), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
-        istudentService.deleteStudentById(id);
+        iStudentService.deleteStudentById(id);
         return ResponseEntity.noContent().build();
     }
 }
