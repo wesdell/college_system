e1f6d9043caa8fc10b7c1b26300cab65178938c7
Sebastian Guerrero <wesdell17@gmail.com>
2025-08-14 18:15:16 -0500

    New College model

---
+++ src/main/java/com/wesdell/college_system/models/College.java
@@ -0,0 +1,38 @@
+package com.wesdell.college_system.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "colleges")
+public class College {
+
+    @Id
+    @SequenceGenerator(name = "college_sequence", sequenceName = "college_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "college_sequence")
+    private Long id;
+
+    @NotBlank(message = "College name is required")
+    @Size(min = 3, max = 100, message = "College name must be between 3 and 100 characters")
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank(message = "College description is required")
+    @Size(min = 3, max = 100, message = "College description must be between 3 and 100 characters")
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "college", cascade = CascadeType.ALL)
+    private List<Faculty> faculties;
+}
