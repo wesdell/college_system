e8f0686414ec7daa4989a7174f789d50b6cd22a2
Sebastian Guerrero <wesdell17@gmail.com>
2025-08-14 19:16:15 -0500

    Implement college operations

---
+++ src/main/java/com/wesdell/college_system/services/CollegeService.java
@@ -0,0 +1,54 @@
+package com.wesdell.college_system.services;
+
+import com.wesdell.college_system.exceptions.ResourceNotFoundException;
+import com.wesdell.college_system.interfaces.ICollegeService;
+import com.wesdell.college_system.models.College;
+import com.wesdell.college_system.repositories.CollegeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CollegeService implements ICollegeService {
+
+    private final CollegeRepository collegeRepository;
+
+    public CollegeService(CollegeRepository collegeRepository) {
+        this.collegeRepository = collegeRepository;
+    }
+
+    @Override
+    public List<College> getColleges() {
+        return collegeRepository.findAll();
+    }
+
+    @Override
+    public College getCollegeById(Long id) throws ResourceNotFoundException {
+        return collegeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("College", id));
+    }
+
+    @Override
+    public College createCollege(College newCollege) {
+        return collegeRepository.save(newCollege);
+    }
+
+    @Override
+    public College updateCollegeById(Long id, College updatedCollege) throws ResourceNotFoundException {
+        College college = collegeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("College", id));
+
+        college.setDescription(updatedCollege.getDescription());
+        college.setFaculties(updatedCollege.getFaculties());
+
+        return collegeRepository.save(college);
+    }
+
+    @Override
+    public void deleteCollegeById(Long id) throws ResourceNotFoundException {
+        College college = collegeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("College", id));
+        collegeRepository.delete(college);
+    }
+
+}
