287b0c1e2c98b139a838fbdf0fed3e5c52f36205
Sebastian Guerrero <wesdell17@gmail.com>
2025-08-25 09:51:30 -0500

    Make global handler inherit from Exception

--- src/main/java/com/wesdell/college_system/exceptions/GlobalException.java
+++ src/main/java/com/wesdell/college_system/exceptions/GlobalException.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalException {
+public class GlobalException extends Exception {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(
